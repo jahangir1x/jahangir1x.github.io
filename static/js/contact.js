f15d1ff8336b478a1f6d2db2b51d60b456db1807
Jahangir Alam Rocky <jahangir64r@gmail.com>
2026-01-18 15:09:37 +0600

    feat: add new portfolio

--- static/js/contact.js
+++
@@ -1,7 +0,0 @@
-const messageArea = document.getElementById("messageArea");
-const messageCounterText = document.getElementById("message-counter");
-messageArea.addEventListener("input", updateMessageCounterText);
-
-function updateMessageCounterText() {
-    messageCounterText.innerHTML = messageArea.value.length + " characters";
-}
